f15892323913c79fac21abac3e1250baaa5293b9
dnguyen <d_nguyen@live.com>
2014-12-03 21:53:38 -0500

    Base code for signal handling

--- 473_mm.c
+++ 473_mm.c
@@ -1,13 +1,49 @@
 #include "473_mm.h"
 
+void *VM_START;
+int VM_SIZE;
+int NUMBER_OF_FRAMES;
+int PAGE_SIZE;
+int POLICY;
+
+static void segv_handler(int sig, siginfo_t *si, void *unused) {
+    printf("sig=%d address=%p\n", sig, si->si_addr);
+    exit(1);
+}
+
 void mm_init(void* vm, int vm_size, int n_frames, int page_size, int policy) {
+    printf("vm=%p vm_size=%d n_frames=%d page_size=%d policy=%d\n", vm, vm_size, n_frames, page_size, policy);
+
+    // Initialize global variables
+    VM_START = vm;
+    VM_SIZE = vm_size;
+    NUMBER_OF_FRAMES = n_frames;
+    PAGE_SIZE = page_size;
+    POLICY = policy;
+
+    // Initialize SIGSEGV handler
+    struct sigaction segv_action;
+    segv_action.sa_flags = SA_SIGINFO;
+    sigemptyset(&segv_action.sa_mask);
+    segv_action.sa_sigaction = segv_handler;
+    if (sigaction(SIGSEGV, &segv_action, NULL) == -1) {
+        printf("sigaction failed\n");
+    }
+
+    // SEGSIGV handler test
+    mprotect(vm, page_size, PROT_NONE);
+    int* test = vm;
+    *(test + 1) = 5;
+
     return;
 }
 
 unsigned long mm_report_npage_faults() {
+    printf("mm_report_npage_faults\n");
     return 0;
 }
 
 unsigned long mm_report_nwrite_backs() {
+    printf("mm_report_nwrite_backs\n");
     return 0;
 }
--- 473_mm.h
+++ 473_mm.h
@@ -1,23 +1,28 @@
 #ifndef _473_MM_H
 #define _473_MM_H
 
-/*
-'mm_init()' initializes the memory management system. 
-'vm' denotes the pointer to the start of virtual address space, 
-'vm_size' denotes the size of the virtual address space, 
-'n_frames' denotes the number of physical pages available in the system, 
-'page_size' denotes the size of both virtual and physical pages, 
-'policy' can take values 1 or 2 -- 1 indicates fifo replacement policy and 2 indicates clock replacement policy. 
-*/
-void mm_init(void* vm, int vm_size, int n_frames, int page_size, int policy); 
+#include <stdio.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/mman.h>
 
 /*
-'mm_report_npage_faults' should return the total number of page faults of the entire system (across all virtual pages). 
+'mm_init()' initializes the memory management system.
+'vm' denotes the pointer to the start of virtual address space,
+'vm_size' denotes the size of the virtual address space,
+'n_frames' denotes the number of physical pages available in the system,
+'page_size' denotes the size of both virtual and physical pages,
+'policy' can take values 1 or 2 -- 1 indicates fifo replacement policy and 2 indicates clock replacement policy.
 */
-unsigned long mm_report_npage_faults(); 
+void mm_init(void* vm, int vm_size, int n_frames, int page_size, int policy);
 
 /*
-'mm_report_nwrite_backs' should return the total number of write backs of the entire system (across all virtual pages). 
+'mm_report_npage_faults' should return the total number of page faults of the entire system (across all virtual pages).
+*/
+unsigned long mm_report_npage_faults();
+
+/*
+'mm_report_nwrite_backs' should return the total number of write backs of the entire system (across all virtual pages).
 */
 unsigned long mm_report_nwrite_backs();
 
